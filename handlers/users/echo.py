51a2f41451f7e92b6dc2efe9dcc77d93aa85c1aa
Ilya <ilya@sinyugin.ru>
2021-07-26 21:47:36 +0200

    working on handlers

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -1,21 +1,20 @@
+from loader import dp, bot
 from aiogram import types
-from aiogram.dispatcher import FSMContext
 
-from loader import dp
+@dp.message_handler(text="/start")
+async def bot_start(message: types.Message):
+    chat_id = message.chat.id
+    text = message.text
+    await bot.send_message(chat_id=chat_id, text=text)
+    await message.answer(text)
+    await message.reply(text)
 
 
-# Эхо хендлер, куда летят текстовые сообщения без указанного состояния
-@dp.message_handler(state=None)
+@dp.message_handler()
 async def bot_echo(message: types.Message):
-    await message.answer(f"Эхо без состояния."
-                         f"Сообщение:\n"
-                         f"{message.text}")
+    chat_id = message.chat.id
+    text = message.text
+    await bot.send_message(chat_id=chat_id, text=text)
+    await message.answer(text)
+    await message.reply(text)
 
-
-# Эхо хендлер, куда летят ВСЕ сообщения с указанным состоянием
-@dp.message_handler(state="*", content_types=types.ContentTypes.ANY)
-async def bot_echo_all(message: types.Message, state: FSMContext):
-    state = await state.get_state()
-    await message.answer(f"Эхо в состоянии <code>{state}</code>.\n"
-                         f"\nСодержание сообщения:\n"
-                         f"<code>{message}</code>")
