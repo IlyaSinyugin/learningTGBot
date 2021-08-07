9dd7bb97deea2527d8e1b1e4146ccc184d198bc9
Ilya <ilya@sinyugin.ru>
2021-08-07 23:10:02 +0200

    throttling added

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -5,11 +5,14 @@ from aiogram.dispatcher.filters.builtin import CommandStart
 
 from filters.test_filter import SomeF
 from loader import dp
+from utils.misc import rate_limit
 
 
+@rate_limit(5, key="start")
 @dp.message_handler(CommandStart(), SomeF())
 async def bot_start(message: types.Message, middleware_data, from_filter):
     await message.answer(f"Привет, {message.from_user.full_name}! \n{middleware_data=}\n{from_filter=}")
     logging.info(f"6. Handler")
     logging.info("Next point: Post Process Message")
     return {"from_handler": "Data from handler"}
+
